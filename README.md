59c552a9dbbd8e24bba67034953a437d4be2ca99
JTPond <jtpond42@gmail.com>
2019-02-11 23:49:02 -0500

    Doc fixes before releasing.

--- README.md
+++ README.md
@@ -2,12 +2,11 @@
 
  A crate for 3-D and 4-D vector and matrix algebra, conceived for use in physics simulations. Builds out from a basic ThreeVec struct including most commonly used operations built in.
 
-## Note
+## Notes
 
-All physics constants are exported in the top in SI units. To retrieve them in Planck or natural units call
- calcify::Consts::planck()
- or
- calcify::Consts::natural()
+* All physics constants are exported in the top in SI units. To retrieve them in Planck or natural units call calcify::Consts::planck() or calcify::Consts::natural().
+
+* FourMat::lambda() has been replaced by fn boost(initial: FourVec, v: ThreeVec). The math has been changed. 
 
 ## todo
 
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,18 @@
 #![crate_name = "calcify"]
 //! # Calcify
 //!
-//! A crate for 3-D vector and matrix algebra, conceived for use in physics simulations. Builds out from a basic ThreeVec struct including most commonly used operations built in
+//!  A crate for 3-D and 4-D vector and matrix algebra, conceived for use in physics simulations. Builds out from a basic ThreeVec struct including most commonly used operations built in.
 //!
-//! ## todo:
+//! ## Notes
+//!
+//! * All physics constants are exported in the top in SI units. To retrieve them in Planck or natural units call calcify::Consts::planck() or calcify::Consts::natural().
+//!
+//! * FourMat::lambda() has been replaced by fn boost(initial: FourVec, v: ThreeVec). The math has been changed.
+//!
+//! ## todo
+//!
 //! Four-Momentum
+
 
 mod four_mat;
 
